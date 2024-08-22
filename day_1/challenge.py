2e92e389489cb229d51858193adf44d1a29343bf
Rafael Silva <rafaelhenriquesilva494@gmail.com>
2024-08-21 22:26:05 -0300

    finish the challenge of day 1

--- day_1/challenge.py
+++ day_1/challenge.py
@@ -1,5 +1,5 @@
 print('Welcome to the Band Name Generator.')
-city_name = input("What's the name of the city the you grew up in?")
-pet_name = input("What's yout pet's name")
+city_name = input("What's the name of the city the you grew up in: ")
+pet_name = input("What's yout pet's name: ")
 band_name = city_name + ' ' + pet_name
-print("Your band name coud be " + band_name)
+print("Your band name could be " + band_name)
