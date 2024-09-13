82abb3927d43c603c04b6351b26579bb0ea132d7
Rafael Silva <rafaelhenriquesilva494@gmail.com>
2024-09-13 17:13:29 -0300

    day 23: finish game turtle crossing

--- 10_99/day_23/turtle-crossing/main.py
+++ 10_99/day_23/turtle-crossing/main.py
@@ -11,6 +11,7 @@ screen.bgcolor('black')
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
@@ -25,11 +26,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
-            
+            scoreboard.game_over()
             
     # Detect successfull crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
             
 screen.exitonclick()
